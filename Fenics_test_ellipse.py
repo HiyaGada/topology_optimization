24d6fef63d380d15fc902f5da7cc7207ff25c353
HiyaGada <63361896+HiyaGada@users.noreply.github.com>
2021-01-23 14:45:43 +0530

    Add files via upload
    Ellipse domain, Poisson equation solved in fenics

---
+++ Fenics_test_ellipse.py
@@ -0,0 +1,81 @@
+"""
+  -Laplace(u) = f    in the unit square
+            u = u_D  on the boundary
+
+  u_D = 1 + x^2 + 2y^2
+    f = -6
+"""
+
+from __future__ import print_function
+from fenics import *
+import matplotlib.pyplot as plt
+
+
+from dolfin import *
+
+# Constructive solid geometry functiontionality
+from mshr import *
+
+# Specify an ellipse geometry
+center = Point(0.0,0.0)
+horizontal_semi_axis = 3.0
+vertical_semi_axis = 1.0
+e = Ellipse(center,
+            horizontal_semi_axis,
+            vertical_semi_axis)
+
+# Generate a mesh from it
+resolution = 10
+mesh = generate_mesh(e,resolution)
+
+# View the mesh:
+from matplotlib import pyplot as plt
+#plot(mesh)
+#plt.show()
+
+# Create mesh and define function space
+#mesh = EllipseMesh(Point(0.0,0.0),[3.0,1.0],0.2)
+V = FunctionSpace(mesh, 'P', 1)
+
+# Define boundary condition
+u_D = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]', degree=2)
+
+def boundary(x, on_boundary):
+    return on_boundary
+
+bc = DirichletBC(V, u_D, boundary)
+
+# Define variational problem
+u = TrialFunction(V)
+v = TestFunction(V)
+f = Constant(-6.0)
+a = dot(grad(u), grad(v))*dx
+L = f*v*dx
+
+# Compute solution
+u = Function(V)
+solve(a == L, u, bc)
+
+# Plot solution and mesh
+plot(u)
+plot(mesh)
+
+# Save solution to file in VTK format
+vtkfile = File('poisson/solution.pvd')
+vtkfile << u
+
+# Compute error in L2 norm
+error_L2 = errornorm(u_D, u, 'L2')
+
+# Compute maximum error at vertices
+vertex_values_u_D = u_D.compute_vertex_values(mesh)
+vertex_values_u = u.compute_vertex_values(mesh)
+import numpy as np
+error_max = np.max(np.abs(vertex_values_u_D - vertex_values_u))
+
+# Print errors
+print('error_L2  =', error_L2)
+print('error_max =', error_max)
+
+# Hold plot
+plt.show()
